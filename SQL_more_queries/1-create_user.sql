11a8effb0c52edb522f4bcf04750c7f8eff382c6
d0-n <d.marume@alustudent.com>
2026-03-25 23:41:55 +0200

    sql_more_queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Create user_0d_1 with all privileges on the server.
--- Create user_0d_1 if missing.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Ensure user_0d_1 password is set as required.
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Drop user_0d_1 if already present to reset privileges.
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+-- Create user_0d_1 with required password.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to user_0d_1.
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
