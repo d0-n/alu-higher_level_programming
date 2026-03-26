0b0a87cba5f56b0f7d4994675bc9e706307b93a7
d0-n <d.marume@alustudent.com>
2026-03-27 00:02:43 +0200

    python-network_0

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request with custom X-HolbertonSchool-User-Id header
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s --header "X-HolbertonSchool-User-Id: 98" "$1"
