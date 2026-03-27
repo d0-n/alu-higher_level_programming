61b70507a71da8596e62f30234b9013bb2ff033b
d0-n <d.marume@alustudent.com>
2026-03-27 14:38:48 +0200

    Fix case-sensitive filters for ORM tasks 2, 3, and 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,7 +18,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(State.name.like("%a%")):
+    for state in session.query(State).filter(
+        State.name.op("LIKE BINARY")("%a%")
+    ):
         session.delete(state)
     session.commit()
 
